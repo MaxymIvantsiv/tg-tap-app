94c85f6de7c265f4cc378166b3f8b2038ebc9b3a
MaxymIvantsiv <90257960+MaxymIvantsiv@users.noreply.github.com>
2024-06-25 02:49:41 +0300

    commit 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,22 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
-<<<<<<< HEAD
-    base: '/'
-=======
-    base: '/tg-tap-app/'
-    // server: {
-    //   port: 443,
-    //   host: "0.0.0.0",
-    //   hmr: {
-    //     host: 'tg-mini-app.local',
-    //     port: 443,
-    //   },
-    //   https: {
-    //     key: './.cert/localhost-key.pem',
-    //     cert: './.cert/localhost.pem',
-    //   },
-    // },
->>>>>>> c924c8cb0a83de1cf849922efc1d39519dc5443d
+  plugins: [react(), svgr()],
+  base: '/tg-tap-app/',
+  // Якщо налаштування сервера необхідні, розкоментуйте наступні рядки
+  // server: {
+  //   port: 443,
+  //   host: "0.0.0.0",
+  //   hmr: {
+  //     host: 'tg-mini-app.local',
+  //     port: 443,
+  //   },
+  //   https: {
+  //     key: './.cert/localhost-key.pem',
+  //     cert: './.cert/localhost.pem',
+  //   },
+  // },
 })
