8a2fa2204574d2bd3cc2629be394231354fa86ab
MaxymIvantsiv <90257960+MaxymIvantsiv@users.noreply.github.com>
2024-06-30 06:58:32 +0300

    try tg connect 9

--- src/hooks/useMainPageHook.ts
+++ src/hooks/useMainPageHook.ts
@@ -2,8 +2,9 @@ import { SERVER_URL } from "../../api/requests.tsx";
 import { User } from "../interfaces/interfaces.ts";
 import { useEffect, useState } from "react";
 import { mockUser } from "../mock/mock-data.ts";
-import {WebAppInitData } from "@twa-dev/types";
-import { WebApp } from '@twa-dev/sdk';
+//import {WebAppInitData } from "@twa-dev/types";
+import WebApp from '@twa-dev/sdk'
+
 export const useMainPageHook = () => {
     const [user, setUser] = useState<User>(mockUser);
 
@@ -21,7 +22,28 @@ export const useMainPageHook = () => {
             console.error("No user to save.");
         }
     };
+	
+	const sendMessage = async (message: string) => {
+    try {
+        const response = await fetch(`${SERVER_URL}/messages`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message }),
+        });
 
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('Response from server:', data);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+};
+	
     const handleButtonTapClick = async () => {
         if (user) {
             if (user.energyPercent >= 1) {
@@ -51,12 +73,12 @@ export const useMainPageHook = () => {
 
     const loadOrCreateUser = async () => {
         let telegramUserId = null;
-		let tgData: WebAppInitData = WebApp.initDataUnsafe;
+		let tgData = WebApp.initDataUnsafe;
 		if (tgData != null && tgData.user != null) {
 			telegramUserId = tgData.user.id;
 		}else
 		{
-			console.error("Init Data Empty");
+			sendMessage('Init Data Empty');
 		}
         const response = await fetch<User[]>(`${SERVER_URL}/users`);
         const users = await response.json();
