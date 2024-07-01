3cf524017b497b8655ff528724f74a783b39e023
MaxymIvantsiv <90257960+AlfaSFS@users.noreply.github.com>
2024-07-01 03:45:01 +0300

    add rating

--- src/pages/rating/Rating.tsx
+++ src/pages/rating/Rating.tsx
@@ -1,13 +1,54 @@
-import {FC, useState} from 'react';
-import {Box, Typography} from '@mui/material';
-import {leaderBoardMock} from '../../mock/mock-data.ts';
+import { FC, useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { leaderBoardMock, LeaderBoardUser } from '../../mock/mock-data.ts';
 import adImage from '../../assets/Любовь-к-автомобилям-шапка4 2.png';
 import RatingListItem from './components/RatingListItem.tsx';
+import { SERVER_URL } from '../../../api/requests.tsx';
 
 interface Props {}
 
 const Rating: FC<Props> = () => {
-  const [ratingData] = useState(leaderBoardMock);
+  const [ratingData, setRatingData] = useState(leaderBoardMock);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(`${SERVER_URL}/users`);
+        const users: LeaderBoardUser[] = await response.json();
+
+        // Додаємо нових користувачів до існуючого масиву ratingData.users
+        const updatedUsers = [...ratingData.users];
+        users.forEach((user, index) => {
+          const newUser = { ...user }; // створюємо новий об'єкт, щоб не змінювати оригінальний
+          newUser.id = user.id;
+          newUser.username = user.name || `User${updatedUsers.length + 1}`;
+          newUser.refAmount = user.balance || 0;
+
+          const existingUser = updatedUsers.find(existingUser => existingUser.id === newUser.id);
+          if (!existingUser) {
+            updatedUsers.push(newUser);
+          }
+        });
+
+        // Сортуємо користувачів за значенням refAmount у спадному порядку
+        updatedUsers.sort((a, b) => b.refAmount - a.refAmount);
+
+        // Встановлюємо позицію кожного користувача після сортування
+        updatedUsers.forEach((user, index) => {
+          user.position = index + 1;
+        });
+
+        setRatingData(prevState => ({
+          ...prevState,
+          users: updatedUsers,
+        }));
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <Box textAlign="center" width="100%">
